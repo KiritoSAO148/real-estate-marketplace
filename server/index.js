b3d5313cc72586ccdeff9d3133331076e1ee855b
Bui Quoc Huy <wuochuy02@gmail.com>
2024-03-30 08:35:35 +0700

    Create sign up API route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -8,7 +9,6 @@ dotenv.config();
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    qa;
     console.log("Connected to MongoDB");
   })
   .catch((err) => {
@@ -17,8 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
