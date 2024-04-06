63913a9f9e2f4a9c3968bc0e2ed37cbe35efdd2a
Bui Quoc Huy <wuochuy02@gmail.com>
2024-04-06 11:54:51 +0700

    Complete update user functionality

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -18,8 +18,7 @@ const userSchema = new mongoose.Schema(
     },
     avatar: {
       type: String,
-      default:
-        "https://www.google.com.vn/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fsearch%2Fprofile%2520picture%2F&psig=AOvVaw1UkTSZCFG_22sEiizuSezJ&ust=1712291158870000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMie8fnbp4UDFQAAAAAdAAAAABAE",
+      default: "../default-avatar-profile.jpg",
     },
   },
   { timestamps: true }
