301136a97af9466793a5d3cbb658f35d462b289a
Bui Quoc Huy <wuochuy02@gmail.com>
2024-04-08 13:59:49 +0700

    Add sign out user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -113,17 +113,13 @@ export default function Profile() {
   const handleSignOut = async () => {
     try {
       dispatch(signOutUserStart());
-
       const res = await fetch("/api/auth/signout");
-
       const data = await res.json();
-
       if (data.success === false) {
         dispatch(signOutUserFailure(data.message));
         return;
       }
-
-      dispatch(signOutUserSuccess());
+      dispatch(deleteUserSuccess(data));
     } catch (error) {
       dispatch(signOutUserFailure(error.message));
     }
