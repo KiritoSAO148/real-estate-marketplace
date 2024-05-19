ba2b9e98ab41c7cf3c9b5423cb9011f6b860645e
Buiqhuy258@ <wuochuy02@gmail.com>
2024-05-19 15:25:13 +0700

    Add admin functionality to the user

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -22,7 +22,10 @@ export const signin = async (req, res, next) => {
     if (!validUser) return next(errorHandler(404, "User not found!"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
     const { password: pass, ...userInfo } = validUser._doc;
     res
       .cookie("access_token", token, { httpOnly: true })
@@ -38,7 +41,10 @@ export const google = async (req, res, next) => {
     const user = await User.findOne({ email: req.body.email });
 
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
+      );
 
       const { password: pass, ...rest } = user._doc;
 
@@ -64,7 +70,10 @@ export const google = async (req, res, next) => {
 
       await newUser.save();
 
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: newUser._id, isAdmin: newUser.isAdmin },
+        process.env.JWT_SECRET
+      );
 
       const { password: pass, ...rest } = newUser._doc;
 
